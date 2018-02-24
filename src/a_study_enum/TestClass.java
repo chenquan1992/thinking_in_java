3e4eb274a98ad7b2ed4906269c966c5e487ff942
chenquan1992 <email>
2018-02-24 10:47:45 +0800

    枚举的学习

---
+++ src/a_study_enum/TestClass.java
@@ -0,0 +1,19 @@
+package a_study_enum;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        String name = Color.BLANK.getName();
+        System.out.println(name);
+
+//        TrafficLight ss = new TrafficLight();
+//        ss.change();
+
+
+
+
+    }
+
+
+
+}
